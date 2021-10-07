5472e730bc579d2b903c2b06913af968e01384e5
Fabian <acostahf4@gmail.com>
2021-10-07 16:58:33 -0500

    staging commit

---
+++ src/components/card.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const data = [
+  {
+    title: "title",
+    content:
+      "Est officia eu deserunt ullamco cupidatat. Ex excepteur ut adipisicing veniam sit. Nulla exercitation culpa minim quis Lorem est consequat ad Lorem sunt. Proident incididunt id laborum quis aute quis consequat nostrud occaecat. Eiusmod ad pariatur fugiat esse tempor. Magna laboris nisi fugiat ullamco voluptate veniam tempor sunt adipisicing tempor dolore velit.",
+  },
+  {
+    title: "title",
+    content:
+      "Est officia eu deserunt ullamco cupidatat. Ex excepteur ut adipisicing veniam sit. Nulla exercitation culpa minim quis Lorem est consequat ad Lorem sunt. Proident incididunt id laborum quis aute quis consequat nostrud occaecat. Eiusmod ad pariatur fugiat esse tempor. Magna laboris nisi fugiat ullamco voluptate veniam tempor sunt adipisicing tempor dolore velit.",
+  },
+  {
+    title: "title",
+    content:
+      "Est officia eu deserunt ullamco cupidatat. Ex excepteur ut adipisicing veniam sit. Nulla exercitation culpa minim quis Lorem est consequat ad Lorem sunt. Proident incididunt id laborum quis aute quis consequat nostrud occaecat. Eiusmod ad pariatur fugiat esse tempor. Magna laboris nisi fugiat ullamco voluptate veniam tempor sunt adipisicing tempor dolore velit.",
+  },
+  {
+    title: "title",
+    content:
+      "Est officia eu deserunt ullamco cupidatat. Ex excepteur ut adipisicing veniam sit. Nulla exercitation culpa minim quis Lorem est consequat ad Lorem sunt. Proident incididunt id laborum quis aute quis consequat nostrud occaecat. Eiusmod ad pariatur fugiat esse tempor. Magna laboris nisi fugiat ullamco voluptate veniam tempor sunt adipisicing tempor dolore velit.",
+  },
+]
+
+export default function card() {
+  return (
+    <div className=" flex flex-col w-96 shadow-lg rounded-lg p-10">
+      {data.map(content => (
+        <div>
+          <div className="flex gap-2">
+            <div className="bg-red-500 rounded-full w-8 flex justify-center items-center p-2">
+              <StaticImage
+                src="../images/icon1.png"
+                //   width={600}
+                quality={95}
+                formats={["auto", "webp", "avif"]}
+                alt="Picture one"
+                // style={{ marginBottom: `1.45rem` }}
+              />
+            </div>
+            <h1 className="text-inline">{content.title}</h1>
+          </div>
+          <div>
+            <p>{content.content}</p>
+          </div>
+          <div className="flex justify-end">
+            <StaticImage
+              src="../images/one.png"
+              //   width={600}
+              quality={95}
+              formats={["auto", "webp", "avif"]}
+              alt="Picture one"
+              style={{ marginBottom: `1.45rem` }}
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
--- src/components/secOne.js
+++ src/components/secOne.js
@@ -1,9 +1,17 @@
 import React from "react"
+import Card from "./card"
 
 const SecOne = () => {
   return (
-    <div>
+    <div className=" bg-nmr-blue px-20">
       <h1>Section 1</h1>
+      <div className="grid grid-cols-3">
+        <div className="">
+          <Card />
+        </div>
+        {/* <div className="pt-10"></div>
+        <div></div> */}
+      </div>
     </div>
   )
 }
