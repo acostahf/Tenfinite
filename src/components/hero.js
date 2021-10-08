fb8fc4b7a4a4fe24770a9121eeecf45171ec205c
Fabian <acostahf4@gmail.com>
2021-10-08 13:03:56 -0500

    changss to hero and subtexts in secFour

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="secOne">
       <div className="hero">
         <div className="gridBlock">
-          <h1 className="title">The representative for Charities</h1>
+          <h1 className="title">A different kind of charity</h1>
           <p className="heroContent">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultricies
             quis lacus vitae sit cras blandit semper. Pulvinar nisi sit
